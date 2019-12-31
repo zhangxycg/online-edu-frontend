f90ee6bbd087b66d9f653ef2a64ab193a8bfb458
zxy <zhangxycg@126.com>
2019-12-31 09:51:54 +0800

    讲师管理前端开发--讲师列表页面实现条件查询

--- src/views/edu/teacher/index.vue
+++ src/views/edu/teacher/index.vue
@@ -1,6 +1,42 @@
 <template>
   <div class="app-container">
     讲师列表
+    <!--查询表单-->
+    <el-form :inline="true" class="demo-form-inline">
+      <el-form-item>
+        <el-input v-model="searchObj.name" placeholder="讲师名"/>
+      </el-form-item>
+
+      <el-form-item>
+        <el-select v-model="searchObj.level" clearable placeholder="讲师头衔">
+          <el-option :value="1" label="高级讲师"/>
+          <el-option :value="2" label="首席讲师"/>
+        </el-select>
+      </el-form-item>
+
+      <el-form-item label="添加时间">
+        <el-date-picker
+          v-model="searchObj.begin"
+          type="datetime"
+          placeholder="选择开始时间"
+          value-format="yyyy-MM-dd HH:mm:ss"
+          default-time="00:00:00"
+        />
+      </el-form-item>
+      <el-form-item>
+        <el-date-picker
+          v-model="searchObj.end"
+          type="datetime"
+          placeholder="选择截止时间"
+          value-format="yyyy-MM-dd HH:mm:ss"
+          default-time="00:00:00"
+        />
+      </el-form-item>
+
+      <el-button type="primary" icon="el-icon-search" @click="getListTeacher()">查询</el-button>
+      <el-button type="default" @click="resetData()">清空</el-button>
+    </el-form>
+
     <!-- 表格 -->
     <el-table
       v-loading="listLoading"
@@ -75,6 +111,7 @@ export default {
   },
   // 写具体的方法调用
   methods: {
+    // 查询讲师数据
     getListTeacher(page=1) {
       this.page = page
       this.listLoading = true
@@ -91,6 +128,11 @@ export default {
         .catch(response => { // 如果请求失败，执行catch里面的操作
           console.log(response)
         })
+    },
+    // 清空方法
+    resetData() { 
+      this.searchObj = {} // 清空数据
+      this.getListTeacher()
     }
   }
 }
