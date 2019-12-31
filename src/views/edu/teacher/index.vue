df201d8b7ec7407cf25233fdd624e82a248f1d15
zxy <zhangxycg@126.com>
2019-12-31 09:17:25 +0800

    讲师管理前端开发--讲师列表页面

--- src/views/edu/teacher/index.vue
+++ src/views/edu/teacher/index.vue
@@ -1,6 +1,47 @@
 <template>
   <div class="app-container">
-    讲师列表,haha
+    讲师列表
+    <!-- 表格 -->
+    <el-table
+      v-loading="listLoading"
+      :data="list"
+      element-loading-text="数据加载中"
+      border
+      fit
+      highlight-current-row>
+
+      <el-table-column
+        label="序号"
+        width="70"
+        align="center">
+        <template slot-scope="scope">
+          {{ (page - 1) * limit + scope.$index + 1 }}
+        </template>
+      </el-table-column>
+
+      <el-table-column prop="name" label="名称" width="80" />
+
+      <el-table-column label="头衔" width="80">
+        <template slot-scope="scope">
+          {{ scope.row.level===1?'高级讲师':'首席讲师' }}
+        </template>
+      </el-table-column>
+
+      <el-table-column prop="intro" label="资历" />
+
+      <el-table-column prop="gmtCreate" label="添加时间" width="160"/>
+
+      <el-table-column prop="sort" label="排序" width="60" />
+
+      <el-table-column label="操作" width="200" align="center">
+        <template slot-scope="scope">
+          <router-link :to="'/edu/teacher/edit/'+scope.row.id">
+            <el-button type="primary" size="mini" icon="el-icon-edit">修改</el-button>
+          </router-link>
+          <el-button type="danger" size="mini" icon="el-icon-delete" @click="removeDataById(scope.row.id)">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 
@@ -10,6 +51,7 @@ export default {
   // 定义变量和初始值
   data() {
     return {
+      listLoading: true, // 是否显示loading信息
       list:null, // 每页数据list集合
       total:0, // 总记录数
       page:1, // 当前页
@@ -26,12 +68,19 @@ export default {
   methods: {
     getListTeacher(page=1) {
       this.page = page
+      this.listLoading = true
       teacher.getTeacherPageList(this.page,this.limit,this.searchObj)
         .then(response => { // 如果请求成功，返回状态码20000，执行then里面的操作
-          console.log(response)
+          // console.log(response)
+          // 每页数据
+          this.list = response.data.items
+          // 总记录数
+          this.total = response.data.total
+
+          this.listLoading = false
         }) 
         .catch(response => { // 如果请求失败，执行catch里面的操作
-
+          console.log(response)
         })
     }
   }
